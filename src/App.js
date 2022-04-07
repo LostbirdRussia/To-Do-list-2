0a357ea2f676e4d5e1ba343696625cc9ddf13dd4
anna <99642325+LostbirdRussia@users.noreply.github.com>
2022-04-07 15:29:41 +0300

    Doctasks

--- src/App.js
+++ src/App.js
@@ -1,27 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import ToDoItem from './ToDoltem/ToDoltem.js';
 import todosData from './todosData.js';
 
-function App() {
-  const handleChange = id => {
+class App extends Component {
+  constructor(){
+   super();
+    this.state = {
+      todoItems : todosData
+    }
+  }
+  handleChange = id => {
     console.log("Значение поменялось", id);
   }
-  const todoItems = todosData.map(item =>{
+  render(){
+    const{todoItems} = this.state;
+    const activeTasks = todoItems.map(item =>{
+      return (
+        <ToDoItem
+          key={item.id} //ключ для списка
+          description={item.description} // передача
+          completed={item.completed}
+          handleChange={() => { this.handleChange(item.id) }} //()вызов функции
+        />
+      )
+    })
     return (
-      <ToDoItem
-        key={item.id} //ключ для списка
-        description={item.description} // передача
-        completed={item.completed}
-        handleChange={() => { handleChange() }} //()вызов функции
-      />
-    )
-  })
-  return (
-    <div className="App">
-      {todoItems}
-    </div>
-  );
+      <div className="App">
+        {activeTasks}
+      </div>
+    );
+  }
 }
 
 export default App;
